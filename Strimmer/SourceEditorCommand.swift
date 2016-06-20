dc65d228b0a4be39501825c575154561ad96b400
Paul Williamson <squarefrog@gmail.com>
2016-06-20 11:44:58 +0100

    Strip all whitespaces in perform function.

--- Strimmer/SourceEditorCommand.swift
+++ Strimmer/SourceEditorCommand.swift
@@ -10,11 +10,36 @@ import Foundation
 import XcodeKit
 
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
-    
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: (NSError?) -> Void ) -> Void {
-        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
-        
+
+        // Keep an array of changed line indices.
+        var changedLineIndexes = [Int]()
+
+        // Loop through each line in the buffer, searching for trailing whitespace and replace only
+        // the trailing whitespace.
+        for lineIndex in 0 ..< invocation.buffer.lines.count {
+            let originalLine = invocation.buffer.lines[lineIndex] as! String
+            let newLine = originalLine.replacingOccurrences(of: "[ \t]+$",
+                                                            with: "",
+                                                            options: .regularExpressionSearch)
+
+            // Only update lines that have changed.
+            if originalLine != newLine {
+                changedLineIndexes.append(lineIndex)
+                invocation.buffer.lines[lineIndex] = newLine
+            }
+        }
+
+        // Select all lines that were replaced.
+        let updatedSelections: [XCSourceTextRange] = changedLineIndexes.map { lineIndex in
+            let lineSelection = XCSourceTextRange()
+            lineSelection.start = XCSourceTextPosition(line: lineIndex, column: 0)
+            lineSelection.end = XCSourceTextPosition(line: lineIndex + 1, column: 0)
+            return lineSelection
+        }
+
+        // Set selections then return with no error.
+        invocation.buffer.selections.setArray(updatedSelections)
         completionHandler(nil)
     }
-    
 }
